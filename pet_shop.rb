306b72493260bb91970818bdd25a9d1fb05f711f
annalanigan <annalanigan@outlook.com>
2017-11-03 11:40:18 +0000

    test stock count passed

--- pet_shop.rb
+++ pet_shop.rb
@@ -15,5 +15,9 @@ def pets_sold(shop)
 end
 
 def increase_pets_sold(shop, value)
-shop[:admin][:pets_sold] += value
+  shop[:admin][:pets_sold] += value
+end
+
+def stock_count(shop)
+  shop[:pets].count
 end
